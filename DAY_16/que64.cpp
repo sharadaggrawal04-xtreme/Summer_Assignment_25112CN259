d383dc520e08a7ac3f32be654b535eaf060c9749
Sharad <sharadaggrawal7@gmail.com>
2026-06-16 12:22:06 +0530

    Add files via upload

---
+++ DAY_16/que64.cpp
@@ -0,0 +1,32 @@
+// Write a program to Remove duplicates from array.
+
+
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    unordered_set<int> seen;
+
+    cout << "Array after removing duplicates: ";
+
+    for (int i = 0; i < n; i++) {
+        if (seen.find(arr[i]) == seen.end()) {
+            cout << arr[i] << " ";
+            seen.insert(arr[i]);
+        }
+    }
+
+    return 0;
+}
